31b07f85b84f29682e33a56456b79b7b85c623d6
abdelrhman mohsen <abdo.mohsen3457643@gmail.com>
2025-06-02 23:55:40 +0200

    add extended user fields and setup related auth tables in database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
+            $table->string('user_name')->unique();
+            $table->string('phone')->nullable();
+            $table->string('whatsapp_number')->nullable();
+            $table->string('address')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('user_image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
